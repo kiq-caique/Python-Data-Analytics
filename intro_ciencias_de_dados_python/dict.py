dddcd14b880b4bfed68570af678fac27f9f62144
kiq-caique <caique-ki@hotmail.com>
2024-03-28 16:58:57 -0300

    dict final

--- intro_ciencias_de_dados_python/dict.py
+++ intro_ciencias_de_dados_python/dict.py
@@ -3,3 +3,23 @@ pessoa = {"nome": "Carlos Henrique", "idade": 31}
 pessoa = dict(nome="Carlos Henrique", idade=31)
 
 print(pessoa["idade"]) # 31
+print(pessoa["nome"]) # Carlos Henrique
+pessoa["telefone"] = "98252-0093"
+print(pessoa) #{'nome': 'Carlos Henrique', 'idade': 31, 'telefone': '98252-0093'}
+
+contatos = {
+    "caique-kiq@hotmail.com":{"nome": "Carlos Henrique", "idade": 31},
+    "mike@hotmail.com":{"nome": "Mike", "idade": 30},
+    "mateus@hotmail.com":{"nome": "Mateus", "idade": 29}
+    }
+
+print(contatos["mike@hotmail.com"]["idade"])
+idade = contatos["mike@hotmail.com"]["idade"]
+print(pessoa["idade"])
+
+for chave in contatos:
+    print(chave, contatos[chave])
+
+# Ou de um jeito melhor com o método *items()*, retorna uma lista de tuplas
+for chave, valor in contatos.items():
+    print(chave, valor)
